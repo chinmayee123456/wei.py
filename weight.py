ae9e2c51484e82c4702f94a51a896ed58184db4e
chinmayee123456 <danyashrichinu@gmail.com>
2025-10-23 15:41:04 +0530

    save

--- weight.py
+++ weight.py
@@ -1,6 +1,13 @@
 weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in meters: "))
 bmi = weight / (height ** 2)
+if bmi < 18.5:
+    category = "Underweight"
+elif 18.5 <= bmi < 25:
+    category = "Normal"
+else:
+    category = "Overweight"
 print("Weight:", weight, "kg")
 print("Height:", height, "m")
 print("BMI:", round(bmi, 1))
+print("Category:", category)
